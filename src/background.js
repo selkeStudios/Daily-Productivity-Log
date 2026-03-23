e690cd9ce61f6fbf2993178e9bb6f3a879083e4f
Selke Studios <selkestudioss@gmail.com>
2026-03-23 18:46:37 -0500

    Implementing Google Chrome's History API
    Implemented Google Chrome's history API through the permissions in the manifest.json as well as a test from Google's official documentation in order to ensure the implementation had worked properly.

--- src/background.js
+++ src/background.js
@@ -1,10 +1,5 @@
 "use strict"
 
-function setBadgeText(enabled) {
-    const text = enabled ? "ON" : "OFF"
-    void chrome.action.setBadgeText({text: text})
-}
-
 function startUp() {
     chrome.storage.sync.get("enabled", (data) => {
         setBadgeText(!!data.enabled)
